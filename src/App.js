80e3f37507e439556fcffb3ac9a64285b27f7fe9
nischal nandurkar <nischalnischal6@gmail.com>
2022-06-10 22:05:10 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          Khudai suru kardo koi blog ka ui dekhk bhadia home page banao ek mai
+          backend deta fir
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
